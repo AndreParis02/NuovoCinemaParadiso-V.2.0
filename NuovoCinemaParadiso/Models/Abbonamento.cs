db33422d62e27ae7cf1c1a60f11210d8b8bc4681
AndreParis02 <paris.andrea14@gmail.com>
2026-04-13 10:00:22 +0200

    Creazione Models/Abbonamento.cs e modifica Models/Utente.cs

---
+++ NuovoCinemaParadiso/Models/Abbonamento.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NuovoCinemaParadiso.Models;
+
+[Table("Abbonamenti")]
+public class Abbonamento
+{
+    [Key]
+    public string Id {get;set;} = Guid.NewGuid().ToString();
+    [Required]
+    [StringLength(50)]
+    public string Nome {get;set;} = string.Empty;
+    [Required]
+    public int Durata {get;set;} 
+    [Required]
+    public DateTime DataInizio { get; set; } = DateTime.UtcNow;
+    public DateTime DataFine { get; set; }
+
+    [Required]
+    public decimal Prezzo {get;set;}
+    [Required]
+    public int Sconto {get;set;}
+    public List<Utente> Utenti {get;set;} = new List<Utente>();
+}
