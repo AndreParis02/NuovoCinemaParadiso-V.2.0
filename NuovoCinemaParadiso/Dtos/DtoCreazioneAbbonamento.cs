9f76d9fa34de4c9d368b12f48d4a3703b987415f
Lollolavio <llavio05@gmail.com>
2026-04-14 09:27:56 +0200

    modifica dto creazione utente

--- NuovoCinemaParadiso/Dtos/DtoCreazioneAbbonamento.cs
+++ NuovoCinemaParadiso/Dtos/DtoCreazioneAbbonamento.cs
@@ -4,8 +4,6 @@ public class DtoCreazioneAbbonamento
 {
     public string Nome { get; set; } = string.Empty;
     public int Durata { get; set; } 
-    public DateTime DataInizio { get; set; }
-    public DateTime DataFine { get; set; }
     public decimal Prezzo { get; set; }
     public int Sconto { get; set; }
 }
