97d95c6eab6e946f8073bb46ac4dc4c46db37ce0
LorrysOnGit <soniccolors05@gmail.com>
2026-04-15 12:38:51 +0200

    creazione e documentazione di 'DtoProiezione'

---
+++ NuovoCinemaParadiso/Dtos/Dtoproiezione.cs
@@ -0,0 +1,9 @@
+namespace NuovoCinemaParadiso.Dtos;
+
+public class DtoProiezione
+{
+    public string Id {get; set; }
+    public string MovieId {get; set; }
+    public string SalaId {get; set; }
+    public string TurnoId {get; set; }
+}
