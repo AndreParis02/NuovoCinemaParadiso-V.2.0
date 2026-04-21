341ea67b9d13696ed11b21c8e51b72e0751d4da0
AndreaBrunoo <andrea.bruno.06.ge@gmail.com>
2026-04-21 12:19:13 +0200

    logService

--- NuovoCinemaParadiso/Services/LogAzioniService.cs
+++ NuovoCinemaParadiso/Services/LogAzioniService.cs
@@ -3,46 +3,69 @@ using NuovoCinemaParadiso.Dtos;
 using NuovoCinemaParadiso.Models;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace NuovoCinemaParadiso.Services;
 
 public class LogAzioniService
 {
   private readonly ContestoDb _contesto;
-  public LogAzioniService(ContestoDb contesto)
+  public LogAzioniService(ContestoDb contesto) 
   {
-    _contesto = contesto;
+    _contesto = contesto; 
   }
+
   
-  public async Task SalvataggioLogAzioneAsync(DtoCreazioneLogAzioni dto)
-  {
-    LogAzioni log = new LogAzioni();
-
-    log.IdUtente = dto.IdUtente;
-    log.NomeAzione = dto.NomeAzione;
-    log.Effettuato = dto.Effettuato;
-    log.Messaggio = dto.Messaggio;
-    log.TimeStamp = DateTimeOffset.UtcNow;
-
-    _contesto.LogAzioni.Add(log);
-    await _contesto.SaveChangesAsync();
-  }
-
-  public async Task<List<DtoLogAzioni>> LetturaLogAzioneAsync()
-  {
-    List<LogAzioni> logs = await _contesto.LogAzioni.ToListAsync();
-    List<DtoLogAzioni> risultati = new List<DtoLogAzioni>();
-    foreach (LogAzioni log in logs)
+    public async Task SalvataggioLogAzioneAsync(DtoCreazioneLogAzioni dto)
     {
-      DtoLogAzioni risultato = new DtoLogAzioni();
-      risultato.Id = log.Id;
-      risultato.IdUtente = log.IdUtente;
-      risultato.NomeAzione = log.NomeAzione;
-      risultato.Effettuato = log.Effettuato;
-      risultato.Messaggio = log.Messaggio;
-      risultato.TimeStamp = log.TimeStamp;
-      risultati.Add(risultato);
+        LogAzioni log = new LogAzioni();
+
+        log.IdUtente = dto.IdUtente;
+        log.NomeAzione = dto.NomeAzione;
+        log.Effettuato = dto.Effettuato;
+        log.Messaggio = dto.Messaggio;
+        log.TimeStamp = DateTimeOffset.UtcNow;
+
+      
+
+        _contesto.LogAzioni.Add(log);
+        await _contesto.SaveChangesAsync();
     }
 
-    return risultati;
+    public async Task SalvataggioLogAzioneAsync(string idUtente, string azione, bool effettuato)
+  {
+      string messaggio = "operazione fallita";
+      if(effettuato) messaggio = "operazione eseguita";
+
+      LogAzioni log = new LogAzioni();
+
+      log.IdUtente = idUtente;
+      log.NomeAzione = azione;
+      log.Effettuato = effettuato;
+      log.Messaggio = messaggio;
+      log.TimeStamp = DateTimeOffset.UtcNow;
+
+        _contesto.LogAzioni.Add(log);
+      await _contesto.SaveChangesAsync();
   }
+
+    public async Task<List<DtoLogAzioni>> LetturaLogAzioneAsync()
+    {
+        List<LogAzioni> logs= await _contesto.LogAzioni.ToListAsync();
+        List<DtoLogAzioni> risultati = new List<DtoLogAzioni>();
+        foreach (LogAzioni log in logs)
+        {
+          DtoLogAzioni risultato = new DtoLogAzioni();
+          risultato.Id = log.Id;
+          risultato.IdUtente = log.IdUtente;
+          risultato.NomeAzione = log.NomeAzione;
+          risultato.Effettuato = log.Effettuato;
+          risultato.Messaggio = log.Messaggio;
+          risultato.TimeStamp = log.TimeStamp;
+          risultati.Add(risultato);
+        }
+
+
+        return risultati;
+    }
+
 }
