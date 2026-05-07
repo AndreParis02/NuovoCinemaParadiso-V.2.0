ec6053c44451546c868a363c6b79e02657ca1087
LorrysOnGit <soniccolors05@gmail.com>
2026-05-07 10:34:57 +0200

    creazione delle pagine dell'archetipo

---
+++ NuovoCinemaParadiso-frontend/src/app/pages/acquisto/acquisto.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acquisto',
+  imports: [],
+  templateUrl: './acquisto.page.html',
+  styleUrl: './acquisto.page.css',
+})
+export class AcquistoPage {}
---
+++ NuovoCinemaParadiso-frontend/src/app/pages/proiezione/proiezione.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proiezione',
+  imports: [],
+  templateUrl: './proiezione.page.html',
+  styleUrl: './proiezione.page.css',
+})
+export class ProiezionePage {}
