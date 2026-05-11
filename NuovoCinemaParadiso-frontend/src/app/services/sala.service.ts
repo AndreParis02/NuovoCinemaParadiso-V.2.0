bb4477ab45e62935b6e9a4c345144b9ed2606cc5
LorrysOnGit <soniccolors05@gmail.com>
2026-05-11 11:33:58 +0200

    creazione e documentazione del sala.service.ts

---
+++ NuovoCinemaParadiso-frontend/src/app/services/sala.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Sala, SalaCreazione  } from '../models/sala.model';
+
+
+@Injectable({ providedIn: 'root' })
+export class SalaService {
+  private readonly http = inject(HttpClient);
+  private readonly baseUrl = `${environment.apiBaseUrl}/sala`;
+
+  ottieniTutti(): Observable<Sala[]> {
+    return this.http.get<Sala[]>(this.baseUrl);
+  }
+  ottieniPerTipologia(tipologiaId: string): Observable<Sala[]> {
+    return this.http.get<Sala[]>(`${this.baseUrl}/tipologia/${tipologiaId}`);
+  }
+
+  ottieniTramiteId(id: string): Observable<Sala> {
+    return this.http.get<Sala>(`${this.baseUrl}/${id}`);
+  }
+
+  crea(payload: SalaCreazione): Observable<Sala> {
+    return this.http.post<Sala>(this.baseUrl, payload);
+  }
+
+  modifica(id: string, payload: SalaCreazione): Observable<Sala> {
+    return this.http.put<Sala>(`${this.baseUrl}/${id}`, payload);
+  }
+
+  elimina(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+}
