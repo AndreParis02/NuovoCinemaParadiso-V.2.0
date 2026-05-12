9aaf9bff3b21c5154ece972a66d66a22a15759af
Lollolavio <llavio05@gmail.com>
2026-05-12 09:50:44 +0200

    feature/service/logazioni.service

---
+++ NuovoCinemaParadiso-frontend/src/app/services/log-azioni.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { LogAzioni, LogAzioniCreazione } from '../models/logAzioni.model';
+
+@Injectable({ providedIn: 'root' })
+export class LogAzioniService {
+
+  private readonly http = inject(HttpClient);
+  private readonly baseUrl = `${environment.apiBaseUrl}/logazioni`;
+
+  salva(payload: LogAzioniCreazione): Observable<void> {
+    return this.http.post<void>(this.baseUrl, payload);
+  }
+
+  ottieniTutti(): Observable<LogAzioni[]> {
+    return this.http.get<LogAzioni[]>(this.baseUrl);
+  }
+}
