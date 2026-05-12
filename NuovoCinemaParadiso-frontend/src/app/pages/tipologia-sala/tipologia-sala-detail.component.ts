5773178f82af6b842ae13609f95aae4fef1f31e2
LorrysOnGit <soniccolors05@gmail.com>
2026-05-12 11:36:20 +0200

    aggiunta detail component e template relativo

---
+++ NuovoCinemaParadiso-frontend/src/app/pages/tipologia-sala/tipologia-sala-detail.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { TipologiaSala } from '../../models/tipologia-sala.model'; // Esempio di path
+import { TipologiaSalaService } from '../../services/tipologia-sala.service';
+
+@Component({
+  selector: 'app-tipologia-sala-detail/{{id}}',
+  templateUrl: './tipologia-sala-detail.component.html',
+})
+export class TipologiaSalaDetailComponent implements OnInit {
+  
+  tipologiaSala: TipologiaSala | null = null;
+  id: string = ''; // impostare questo ID in qualche modo, ad esempio tramite route parametrica
+
+  // L'iniezione del servizio nel costruttore
+  constructor(private tipologiaSalaService: TipologiaSalaService, id: string) {
+    this.id = id;
+  }
+
+  ngOnInit(): void {
+    this.tipologiaSalaService.ottieniTramiteId(this.id).subscribe(data => {
+      this.tipologiaSala = data;
+    });
+  }
+}
