83aa2c9a92b4a199d0ae4e2aabb85c3d5364bf92
AndreParis02 <paris.andrea14@gmail.com>
2026-05-12 11:33:46 +0200

    utente.service.ts

---
+++ NuovoCinemaParadiso-frontend/src/app/services/utente.service.ts
@@ -0,0 +1,38 @@
+import { environment } from '../../environments/environment';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Utente, UtenteCreazione } from '../models/utente.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UtenteService {
+
+    private readonly http = inject(HttpClient);
+    private readonly baseUrl = `${environment.apiBaseUrl}/utente`;
+
+
+    profilo(id: string): Observable<Utente> {
+        return this.http.get<Utente>(`${this.baseUrl}/${id}`);
+    }
+
+    modifica(payload: UtenteCreazione): Observable<Utente> {
+        return this.http.put<Utente>(this.baseUrl, payload);
+    }
+
+    elimina(id: string): Observable<void> {
+        return this.http.delete<void>(`${this.baseUrl}/${id}`);
+    }
+
+    abbonati(abbonamentoId: string, utenteId: string): Observable<Utente> {
+
+        return this.http.post<Utente>(`${this.baseUrl}/abbonati`,{abbonamentoId,utenteId});
+    }
+
+    giftcard(giftcardId: string, utenteId: string): Observable<Utente> {
+
+        return this.http.post<Utente>(`${this.baseUrl}/giftcard`,{giftcardId,utenteId});
+    }
+
+}
