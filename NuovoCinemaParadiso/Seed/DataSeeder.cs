0b39d13f40ffdaa2a0f7b911e21b991923b4a2f9
AndreaBrunoo <andrea.bruno.06.ge@gmail.com>
2026-05-25 09:15:38 +0200

    test

--- NuovoCinemaParadiso/Seed/DataSeeder.cs
+++ NuovoCinemaParadiso/Seed/DataSeeder.cs
@@ -25,8 +25,10 @@ public static class DataSeeder
             "gestore@gmail.com",
             "123456",
             "Gestore",
-            60
-            , false);
+            60,
+            false,
+            1000
+            );
 
         Utente operatore = await AssicuraEsistenzaUtenteAsync(
             gestioneUtenti,
@@ -34,7 +36,8 @@ public static class DataSeeder
             "123456",
             "Operatore",
             35,
-            false);
+            false,
+            1000);
 
         Utente utente = await AssicuraEsistenzaUtenteAsync(
         gestioneUtenti,
@@ -42,7 +45,8 @@ public static class DataSeeder
             "123456",
             "Utente Uno",
             15,
-            true);
+            true,
+            1000);
 
         ContoCinema contoCinema = await AssicuraEsistenzaContoCinema ( contestoDb, "IT60X0542811101000000123456", "Gestore", 200);
 
@@ -81,9 +85,9 @@ public static class DataSeeder
         var sala2 = await AssicuraEsistenzaSala(contestoDb, "Sala2", 40, tipologia3D.Id);
         var sala3 = await AssicuraEsistenzaSala(contestoDb, "Sala3", 50, tipologiaImax.Id);
 
-        await AssicuraEsistenzaAbbonamento(contestoDb, "Mensile", 70, 25, 1);
-        await AssicuraEsistenzaAbbonamento(contestoDb, "Semestrale", 210, 50, 6);
-        await AssicuraEsistenzaAbbonamento(contestoDb, "Annuale", 300, 75, 12);
+        await AssicuraEsistenzaAbbonamento(contestoDb, "Mensile", 70, 2, 1);
+        await AssicuraEsistenzaAbbonamento(contestoDb, "Semestrale", 210, 4, 6);
+        await AssicuraEsistenzaAbbonamento(contestoDb, "Annuale", 300, 7, 12);
 
         var proiezione1 = await AssicuraEsistenzaProiezione(contestoDb, new DateOnly(2027, 1, 1), movie1.Id, sala1.Id, turnoMattina.Id);
         var proiezione2 = await AssicuraEsistenzaProiezione(contestoDb, new DateOnly(2027, 1, 1), movie2.Id, sala2.Id, turnoPomeriggio.Id);
@@ -92,7 +96,6 @@ public static class DataSeeder
         await AssicuraEsistenzaBiglietto(contestoDb, proiezione1.Id, utente.Id, 1, new DateTimeOffset(DateTime.Now), 10);
         await AssicuraEsistenzaBiglietto(contestoDb, proiezione2.Id, utente.Id, 2, new DateTimeOffset(DateTime.Now), 20);
         await AssicuraEsistenzaBiglietto(contestoDb, proiezione3.Id, utente.Id, 3, new DateTimeOffset(DateTime.Now), 30);
-
     }
 
     private static async Task AssicuraEsistenzaRuoloAsync(RoleManager<IdentityRole> managerRuolo, string nomeRuolo)
@@ -113,7 +116,8 @@ public static class DataSeeder
         string password,
         string nomeCompleto,
         int eta,
-        bool abbonato
+        bool abbonato,
+        int saldo
         )
     {
         Utente? utenteEsistente = await gestioneUtenti.FindByEmailAsync(email);
@@ -130,6 +134,7 @@ public static class DataSeeder
         utente.Eta = eta;
         utente.SeAbbonato = abbonato;
         utente.AbbonamentoId = null;
+        utente.Saldo = saldo;
 
         IdentityResult risultato = await gestioneUtenti.CreateAsync(utente, password);
 
