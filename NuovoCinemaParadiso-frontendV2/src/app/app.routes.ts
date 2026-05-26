3620b4ed69699c26b70420a201e7fd6475ae437e
latorracasimeone <latorracasimeone@gmail.com>
2026-05-26 11:28:27 +0200

    aggiunta al frontend della pagina dei log di sistema (log azioni) visionabile esclusivamente da gestore

--- NuovoCinemaParadiso-frontendV2/src/app/app.routes.ts
+++ NuovoCinemaParadiso-frontendV2/src/app/app.routes.ts
@@ -4,34 +4,39 @@ import { guestGuard } from './core/guards/guest.guard';
 import { roleGuard } from './core/guards/role.guard';
 
 export const routes: Routes = [
-    {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'dashboard'
-    },
-    {
-        path: 'login',
-        canActivate: [guestGuard],
-        loadComponent: () => import('./pages/login/login.page').then((m) => m.LoginPage) // load component carica le pagine in lazyloagin nel modello standalone di angular
-    },
-    /*
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard',
+  },
+  {
+    path: 'login',
+    canActivate: [guestGuard],
+    loadComponent: () => import('./pages/login/login.page').then((m) => m.LoginPage), // load component carica le pagine in lazyloagin nel modello standalone di angular
+  },
+  /*
     {
         path: 'register',
         canActivate: [guestGuard],
         loadComponent: () => import('./pages/register/register.page').then((m) => m.RegisterPage)
     },
      */
-    {
-        path: 'movie',
-        canActivate: [authGuard],
-        loadComponent: () => import('./pages/movie/movie.page').then((m) => m.MoviePage)
-    },
-    {
-        path: 'dashboard',
-        canActivate: [authGuard],
-        loadComponent: () => import('./pages/dashboard/dashboard.page').then((m) => m.DashboardPage)
-    },
-    /*
+  {
+    path: 'movie',
+    canActivate: [authGuard],
+    loadComponent: () => import('./pages/movie/movie.page').then((m) => m.MoviePage),
+  },
+  {
+    path: 'dashboard',
+    canActivate: [authGuard],
+    loadComponent: () => import('./pages/dashboard/dashboard.page').then((m) => m.DashboardPage),
+  },
+  {
+    path: 'gestore/logs',
+    canActivate: [authGuard],
+    loadComponent: () => import('./pages/log-azioni/log-azioni.page').then((m) => m.LogAzioniPage),
+  },
+  /*
     {
         path: 'abbonamento/:id',
         canActivate: [authGuard],
@@ -60,13 +65,16 @@ export const routes: Routes = [
         loadComponent: () => import('./pages/users-list/users-list.page').then((m) => m.BigliettoDetailPage)
     },
     */
-    {
-        path: 'gestore/change-role',
-        canActivate: [authGuard, roleGuard],
-        data: { roles: ['gestore'] },
-        loadComponent: () => import('./pages/gestore-change-role/gestore-change-role.page').then((m) => m.GestoreChangeRolePage)
-    },
-    /*
+  {
+    path: 'gestore/change-role',
+    canActivate: [authGuard, roleGuard],
+    data: { roles: ['gestore'] },
+    loadComponent: () =>
+      import('./pages/gestore-change-role/gestore-change-role.page').then(
+        (m) => m.GestoreChangeRolePage,
+      ),
+  },
+  /*
     {
         path: 'biglietto',
         canActivate: [authGuard],
@@ -93,17 +101,23 @@ export const routes: Routes = [
         loadComponent: () => import('./pages/genere-movie/genere-movie.page').then((m) => m.GenereMovieListPage)
     },
     */
-    {
-        path: 'tipologia-sala',
-        canActivate: [authGuard],
-        loadComponent: () => import('../../../NuovoCinemaParadiso-frontendV2/src/app/pages/tipologia-sala/tipologia-sala.page').then((m) => m.TipologiaSalaPage)
-    },
-        {
-        path: 'genere-movie',
-        canActivate: [authGuard],
-        loadComponent: () => import('../../../NuovoCinemaParadiso-frontendV2/src/app/pages/genere-movie/genere-movie.page').then((m) => m.GenereMoviePage)
-    },
-    /*
+  {
+    path: 'tipologia-sala',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('../../../NuovoCinemaParadiso-frontendV2/src/app/pages/tipologia-sala/tipologia-sala.page').then(
+        (m) => m.TipologiaSalaPage,
+      ),
+  },
+  {
+    path: 'genere-movie',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('../../../NuovoCinemaParadiso-frontendV2/src/app/pages/genere-movie/genere-movie.page').then(
+        (m) => m.GenereMoviePage,
+      ),
+  },
+  /*
     {
         path: 'tipologia-sala',
         //canActivate: [authGuard],
@@ -148,4 +162,3 @@ export const routes: Routes = [
 
 */
 ];
-
