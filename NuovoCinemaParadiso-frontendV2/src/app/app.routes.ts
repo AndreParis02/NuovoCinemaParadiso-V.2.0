a3613fa097a581953dca24c549aaf2f1def7a5b3
LorrysOnGit <soniccolors05@gmail.com>
2026-05-29 10:25:46 +0200

    pulizzia routes

--- NuovoCinemaParadiso-frontendV2/src/app/app.routes.ts
+++ NuovoCinemaParadiso-frontendV2/src/app/app.routes.ts
@@ -16,44 +16,14 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/login/login.page').then((m) => m.LoginPage),
   },
   {
-        path: 'register',
-        canActivate: [guestGuard],
-        loadComponent: () => import('./pages/register/register.page').then((m) => m.RegisterPage)
     path: 'register',
     canActivate: [guestGuard],
     loadComponent: () => import('./pages/register/register.page').then((m) => m.RegisterPage),
   },
-
   {
     path: 'profilo',
     canActivate: [authGuard],
     loadComponent: () => import('./pages/utente/utente.page').then((m) => m.UtentePage),
-
-    {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'dashboard',
-    },
-    {
-        path: 'login',
-        canActivate: [guestGuard],
-        loadComponent: () => import('./pages/login/login.page').then((m) => m.LoginPage),
-    },
-    {
-        path: 'register',
-        canActivate: [guestGuard],
-        loadComponent: () => import('./pages/register/register.page').then((m) => m.RegisterPage)
-    },
-  */
-  {
-    path: 'profilo',
-    canActivate: [authGuard],
-    loadComponent: () => import('./pages/utente/utente.page').then((m) => m.UtentePage), 
-  },
-  {
-    path: 'movie',
-    canActivate: [authGuard],
-    loadComponent: () => import('./pages/movie/movie.page').then((m) => m.MoviePage),
   },
   {
     path: 'dashboard',
@@ -69,88 +39,23 @@ export const routes: Routes = [
     canActivate: [roleGuard], // <-- Solo roleGuard
     data: { roles: ['Gestore'] }, // <-- Ruolo richiesto
   },
-
-  {
-    path: 'movie',
-    canActivate: [authGuard],
-    loadComponent: () => import('./pages/movie/movie.page').then((m) => m.MoviePage),
-  },
-  
-    {
-        path: 'movie',
-        canActivate: [authGuard],
-        loadComponent: () => import('./pages/movie/movie.page').then((m) => m.MoviePage),
-    },
-    {
-        path: 'dashboard',
-        canActivate: [authGuard],
-        data: {
-            roles: ['Operatore']
-        },
-        loadComponent: () => import('./pages/dashboard/dashboard.page').then((m) => m.DashboardPage),
-    },
-    {
-        path: 'gestore/logs',
-        canActivate: [authGuard],
-        loadComponent: () => import('./pages/log-azioni/log-azioni.page').then((m) => m.LogAzioniPage),
-    },
-    {
-        path: 'dashboard',
-        canActivate: [authGuard],
-        loadComponent: () => import('./pages/dashboard/dashboard.page').then((m) => m.DashboardPage)
-    },
-    {
-        path: 'abbonamento',
-        canActivate: [authGuard],
-        loadComponent: () => import('./pages/abbonamento/abbonamento.page').then((m) => m.AbbonamentoPage)
-    },
-
-    {
-        path: 'operatore/cambio-ruolo',
-        canActivate: [authGuard, roleGuard],
-        data: { roles: ['operatore'] },
-        loadComponent: () => import('./pages/operatore/operatore.page').then((m) => m.OperatorePage)
-    },
-    {
-        path: 'abbonamenti',
-        canActivate: [authGuard],
-        loadComponent: () => import('./pages/abbonamento/abbonamento.page').then((m) => m.AbbonamentoPage)
-    },
-    /*
-  {
-    path: 'dashboard',
-    canActivate: [authGuard],
-    loadComponent: () => import('./pages/dashboard/dashboard.page').then((m) => m.DashboardPage),
-  },
   {
     path: 'abbonamento',
     canActivate: [authGuard],
-    loadComponent: () =>
-      import('./pages/abbonamento/abbonamento.page').then((m) => m.AbbonamentoPage),
+    loadComponent: () => import('./pages/abbonamento/abbonamento.page').then((m) => m.AbbonamentoPage)
   },
 
   {
-    path: 'operatore/change-role',
+    path: 'operatore/cambio-ruolo',
     canActivate: [authGuard, roleGuard],
-    data: { roles: ['gestore'] },
-    loadComponent: () =>
-      import('./pages/gestore-change-role/gestore-change-role.page').then(
-        (m) => m.GestoreChangeRolePage,
-      ),
+    data: { roles: ['operatore'] },
+    loadComponent: () => import('./pages/operatore/operatore.page').then((m) => m.OperatorePage)
   },
   {
     path: 'abbonamenti',
     canActivate: [authGuard],
-    loadComponent: () =>
-      import('./pages/abbonamento/abbonamento.page').then((m) => m.AbbonamentoPage),
+    loadComponent: () => import('./pages/abbonamento/abbonamento.page').then((m) => m.AbbonamentoPage)
   },
-  /*
-    {
-        path: 'abbonamento-list',
-        canActivate: [authGuard],
-        loadComponent: () => import('./pages/abbonamento/abbonamento-list.page').then((m) => m.AbbonamentoListPage)
-    },
-   */
   {
     path: 'listaUtenti',
     canActivate: [authGuard],
@@ -159,149 +64,49 @@ export const routes: Routes = [
         (m) => m.OperatoreListaUtentiPage,
       ),
   },
-  /*
-    {
-        path: 'gestore/biglietto',
-        canActivate: [authGuard],
-        loadComponent: () => import('./pages/users-list/users-list.page').then((m) => m.BigliettoListPage)
-    },
-    
-    {
-        path: 'gestore/biglietto:id',
-        canActivate: [authGuard],
-        loadComponent: () => import('./pages/users-list/users-list.page').then((m) => m.BigliettoDetailPage)
-    },
-    */
-  {
-    path: 'gestore/change-role',
-    canActivate: [authGuard, roleGuard],
-    data: { roles: ['gestore'] },
-    loadComponent: () =>
-      import('./pages/gestore-change-role/gestore-change-role.page').then(
-        (m) => m.GestoreChangeRolePage,
-      ),
-  },
-  /*
-  */
-    {
-        path: 'proiezioni',
-        canActivate: [authGuard, roleGuard],
-        loadComponent: () => import('./pages/proiezione/proiezione.page').then((m) => m.ProiezionePage)
-    },
-    {
-        path: 'film',
-        canActivate: [authGuard, roleGuard],
-        loadComponent: () => import('./pages/movie/movie.page').then((m) => m.MoviePage)
-    },
-    {
-        path: 'biglietto',
-        canActivate: [authGuard],
-        loadComponent: () => import('./pages/biglietto/biglietto.page').then((m) => m.BigliettoPage)
-    },
-    {
-        path: 'giftcard',
-        canActivate: [authGuard],
-        loadComponent: () => import('./pages/giftcard/giftcard.page').then((m) => m.GiftcardPage)
-    },
-    {
-        path: 'genere-movie',
-        canActivate: [authGuard],
-        loadComponent: () => import('./pages/genere-movie/genere-movie.page').then((m) => m.GenereMoviePage)
-    },
-    {
-        path: 'tipologia-sala',
-        canActivate: [authGuard],
-        loadComponent: () => import('../../../NuovoCinemaParadiso-frontendV2/src/app/pages/tipologia-sala/tipologia-sala.page').then((m) => m.TipologiaSalaPage)
-    },
-    {
-        path: 'genere-movie',
-        canActivate: [authGuard],
-        loadComponent: () => import('../../../NuovoCinemaParadiso-frontendV2/src/app/pages/genere-movie/genere-movie.page').then((m) => m.GenereMoviePage)
-    },
-    /*
-   */
   {
     path: 'proiezioni',
     canActivate: [authGuard, roleGuard],
-    loadComponent: () => import('./pages/proiezione/proiezione.page').then((m) => m.ProiezionePage),
-  },
-  {
-    path: 'film',
-    canActivate: [authGuard, roleGuard],
-    loadComponent: () => import('./pages/movie/movie.page').then((m) => m.MoviePage),
+    loadComponent: () => import('./pages/proiezione/proiezione.page').then((m) => m.ProiezionePage)
   },
   {
     path: 'biglietto',
     canActivate: [authGuard],
-    loadComponent: () => import('./pages/biglietto/biglietto.page').then((m) => m.BigliettoPage),
+    loadComponent: () => import('./pages/biglietto/biglietto.page').then((m) => m.BigliettoPage)
   },
   {
     path: 'giftcard',
     canActivate: [authGuard],
-    loadComponent: () => import('./pages/giftcard/giftcard.page').then((m) => m.GiftcardPage),
+    loadComponent: () => import('./pages/giftcard/giftcard.page').then((m) => m.GiftcardPage)
   },
   {
     path: 'genere-movie',
     canActivate: [authGuard],
-    loadComponent: () =>
-      import('./pages/genere-movie/genere-movie.page').then((m) => m.GenereMoviePage),
+    loadComponent: () => import('./pages/genere-movie/genere-movie.page').then((m) => m.GenereMoviePage)
   },
   {
     path: 'tipologia-sala',
     canActivate: [authGuard],
-    loadComponent: () =>
-      import('../../../NuovoCinemaParadiso-frontendV2/src/app/pages/tipologia-sala/tipologia-sala.page').then(
-        (m) => m.TipologiaSalaPage,
-      ),
+    loadComponent: () => import('../../../NuovoCinemaParadiso-frontendV2/src/app/pages/tipologia-sala/tipologia-sala.page').then((m) => m.TipologiaSalaPage)
   },
   {
     path: 'genere-movie',
     canActivate: [authGuard],
-    loadComponent: () =>
-      import('../../../NuovoCinemaParadiso-frontendV2/src/app/pages/genere-movie/genere-movie.page').then(
-        (m) => m.GenereMoviePage,
-      ),
+    loadComponent: () => import('../../../NuovoCinemaParadiso-frontendV2/src/app/pages/genere-movie/genere-movie.page').then((m) => m.GenereMoviePage)
   },
-  /*
-    {
-        path: 'tipologia-sala',
-        //canActivate: [authGuard],
-        loadComponent: () => import('./pages/tipologia-sala/tipologia-sala-list.component').then((m) => m.TipologiaSalaListComponent)
-    },
-    {
-        path: '**',
-        redirectTo: 'dashboard'
-    },
-]
-*/
   {
     path: 'sala',
     canActivate: [authGuard],
     loadComponent: () => import('./pages/sala/sala.page').then((m) => m.SalaPage)
-},
-/*
-{
-    path: 'proiezione',
+  },
+  {
+    path: 'movies',
     canActivate: [authGuard],
-    loadComponent: () => import('./pages/proiezione/proiezione.page').then((m) => m.ProiezioneDetailPage)
-},
-{
-    path: 'proiezione',
-    canActivate: [authGuard],
-    loadComponent: () => import('./pages/proiezione/proiezione.page').then((m) => m.ProiezioneListPage)
-},
-*/
+    loadComponent: () => import('./pages/movie/movie.page').then((m) => m.MoviePage),
+  },
   {
     path: 'turno',
     canActivate: [authGuard],
     loadComponent: () => import('./pages/turno/turno.page').then((m) => m.TurnoPage),
   },
-  /*
-{
-    path: 'turno',
-    canActivate: [authGuard],
-    loadComponent: () => import('./pages/turno/turno.page').then((m) => m.TurnoListPage)
-},
-
-*/
 ];
