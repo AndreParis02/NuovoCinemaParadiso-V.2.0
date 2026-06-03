e8c4f7c84e467d4493dd1aabec63529c847350f7
AndreParis02 <paris.andrea14@gmail.com>
2026-06-03 10:31:00 +0200

    creazione component abbonamento-list

---
+++ NuovoCinemaParadiso-frontendV2/src/app/features/abbonamento/components/abbonamento-list.component.ts
@@ -0,0 +1,62 @@
+import { Component, inject, signal } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+
+import { AbbonamentoService } from '../../../services/abbonamento.service';
+import { Abbonamento } from '../../../models/abbonamento.model';
+
+@Component({
+    selector: 'app-abbonamento-list',
+    standalone: true,
+    templateUrl: './abbonamento-list.component.html',
+    styleUrls: ['./abbonamento-list.component.css']
+})
+export class AbbonamentoListComponent {
+
+    private readonly abbonamentoService = inject(AbbonamentoService);
+
+
+
+    readonly abbonamenti = signal<Abbonamento[]>([]);
+    readonly staCaricando = signal(false);
+    readonly messaggioErrore = signal('');
+    readonly messaggioSuccesso = signal('');
+
+    constructor() {
+        this.caricaAbbonamenti();
+    }
+
+    caricaAbbonamenti(): void {
+
+        this.staCaricando.set(true);
+        this.messaggioErrore.set('');
+
+        this.abbonamentoService.ottieniTutto().subscribe({
+            next: (items) => {
+                this.abbonamenti.set(items);
+                this.staCaricando.set(false);
+            },
+            error: (error: unknown) => {
+                this.staCaricando.set(false);
+                this.messaggioErrore.set(
+                    this.estraiMessaggioErrore(error, 'Abbonamenti non trovati')
+                );
+            }
+        });
+    }
+
+    tracciaPerId(_: number, item: Abbonamento): string {
+        return item.id;
+    }
+
+    private estraiMessaggioErrore(
+        error: unknown,
+        fallback: string
+    ): string {
+
+        if (error instanceof HttpErrorResponse) {
+            return error.error?.message ?? fallback;
+        }
+
+        return fallback;
+    }
+}
