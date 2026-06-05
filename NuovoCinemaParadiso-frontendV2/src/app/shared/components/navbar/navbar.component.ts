b32b0f04e476898f82f306c894a559227a040265
FabTam <fabiotamma90@gmail.com>
2026-06-05 12:11:04 +0200

    Modifiche navbar

--- NuovoCinemaParadiso-frontendV2/src/app/shared/components/navbar/navbar.component.ts
+++ NuovoCinemaParadiso-frontendV2/src/app/shared/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, computed, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from './../../../services/auth.service';
+import { UtenteService } from '../../../services/utente.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,13 +11,18 @@ import { AuthService } from './../../../services/auth.service';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
   private readonly authService = inject(AuthService);
+  private readonly utenteService = inject(UtenteService);
 
-  readonly utente          = this.authService.utenteCorrente;
-  readonly isAutenticato   = computed(() => this.authService.isAutenticato ());
-  readonly isGestore       = computed(() => this.authService.isGestore ());
+  readonly utenteCorrente = this.utenteService.profilo;
+
+  readonly isAutenticato = computed(() => this.authService.isAutenticato());
+  readonly isGestore = computed(() => this.authService.isGestore());
+  readonly isOperatore = computed(() => this.authService.isOperatore());
 
 
+  
   logout(): void {
     this.authService.logout();
   }
