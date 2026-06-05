b1a8cddd6dfbdba8dae14912caff1f0f19b909d8
Marco <marcomarco85@hotmail.it>
2026-06-05 11:38:46 +0200

    modifica servizio abbonati

--- NuovoCinemaParadiso/Services/UtenteService.cs
+++ NuovoCinemaParadiso/Services/UtenteService.cs
@@ -79,12 +79,21 @@ public class UtenteService
             return (false, "Utente non trovato.");
 
         if (utenteTrovato.SeAbbonato)
-            return (false, "L'utente è già abbonato.");
-
+        {
+            DateTimeOffset dataScadenzaAbbonamento = utenteTrovato.DataInizioAbbonamento.AddMonths(abbonamentoTrovato.Durata);
+            if (DateTimeOffset.UtcNow < dataScadenzaAbbonamento)
+                return (false, "L'utente è già abbonato.");
+        }
         if (utenteTrovato.Saldo < abbonamentoTrovato.Prezzo)
             return (false, "Credito insufficiente per abbonarsi.");
 
-        utenteTrovato.Saldo -= abbonamentoTrovato.Prezzo;
+        //aggiornamento dei saldi utente e conto cinema
+        var contoCinema = await _contesto.ContoCinema.FirstOrDefaultAsync();
+        var saldi = await Calcoli.CalcolaSaldo(abbonamentoTrovato.Prezzo, utenteTrovato, contoCinema);
+        utenteTrovato.Saldo = saldi[0];
+        contoCinema.Saldo = saldi[1];
+
+        utenteTrovato.TipologiaAbbonamento = abbonamentoTrovato.Nome;
         utenteTrovato.AbbonamentoId = abbonamentoTrovato.Id;
         utenteTrovato.SeAbbonato = true;
         utenteTrovato.DataInizioAbbonamento = DateTimeOffset.UtcNow;
