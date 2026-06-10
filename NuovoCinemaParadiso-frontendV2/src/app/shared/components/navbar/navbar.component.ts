4472093ad242d98bd8dfc63ce722e3f73a322c76
FabTam <fabiotamma90@gmail.com>
2026-06-10 12:41:59 +0200

    Modifiche navbar

--- NuovoCinemaParadiso-frontendV2/src/app/shared/components/navbar/navbar.component.ts
+++ NuovoCinemaParadiso-frontendV2/src/app/shared/components/navbar/navbar.component.ts
@@ -14,19 +14,17 @@ export class NavbarComponent {
     private readonly authService = inject(AuthService);
     private readonly navbarSharedStateService = inject(NavbarSharedStateService);
 
-    // Mappiamo i segnali dell'AuthService direttamente per l'HTML
-    readonly isAutenticato = this.authService.isAutenticato;
-    readonly isGestore = this.authService.isGestore;
-    readonly isOperatore = this.authService.isOperatore;
+    readonly isAutenticato = computed(()=> this.authService.isAutenticato());
+    readonly isGestore = computed(()=> this.authService.isGestore());
+    readonly isOperatore = computed(()=> this.authService.isOperatore());
+    readonly isUtente = computed(()=> this.authService.isUtente());
     
-    // Leggiamo passivamente l'utente e calcoliamo il saldo in tempo reale
     readonly utenteCorrente = this.navbarSharedStateService.utenteLoggato;
     readonly utenteInSessione = this.authService.utenteCorrente;
     readonly saldoCondiviso = computed(() => this.utenteCorrente()?.saldo ?? 0);
 
     logout(): void {
         this.authService.logout(); 
-        // Non serve chiamare pulisciUtente()! 
-        // L'effect nel servizio noterà che sessioneAttiva è diventata null e pulirà tutto da solo.
+      
     }
 }
